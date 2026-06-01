89db98e3188cd18ab88b98b215f07139cb963964
chahrazed soltani <chahrazed.sltn@gmail.com>
2026-06-01 11:28:36 +0200

    TP3 - Pokédex complet (étapes 6-10) : routes, navbar, pages

--- TP3/src/app/app.ts
+++ TP3/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FavorisService } from './services/favoris.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('pokedex');
+  favoris = inject(FavorisService);
 }
